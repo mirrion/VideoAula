7a5e602de8257ac2a9add7b61c4bf5551ed1e7f0
mirrion <mirrion@gmail.com>
2019-02-11 17:04:19 -0200

    Modelos permissao de usuarios

---
+++ SistemaLoja/ViewModels/RoleView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaLoja.ViewModels
+{
+    public class RoleView
+    {
+        public string RoleId { get; set; }
+        public string Name { get; set; }
+    }
+}
