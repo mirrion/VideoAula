46c947ddbe2bb0ddfcdd90685a629fe41782caea
mirrion <mirrion@gmail.com>
2019-02-12 16:30:38 -0200

    AjaxConceito criando Controoller e iniciando view

---
+++ SistemaLoja/Controllers/AJAXConceitoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaLoja.Controllers
+{
+    public class AJAXConceitoController : Controller
+    {
+        // GET: AJAXConceito
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult JsonFatorial(int n)
+        {
+            if (!Request.IsAjaxRequest())
+            {
+                return null;
+            }
+            var result = new JsonResult
+            {
+                Data = new { Fatorial = Fatorial(n) }
+            };
+            return result;
+        }
+
+        private double Fatorial(int n)
+        {
+            double fatorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                fatorial *= i;
+            }
+            return fatorial;
+        }
+    }
+}
