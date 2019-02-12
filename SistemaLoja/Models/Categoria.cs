f1bc68e83a6900c875fe4d7017dd692e9b9ef613
mirrion <mirrion@gmail.com>
2019-02-12 18:31:55 -0200

    Cria Modelo Categoria

---
+++ SistemaLoja/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaLoja.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [Display(Name = "Tipo de Documento")]
+        [Required(ErrorMessage = "Você precisa entrar com o {0}")]
+        public string Descricao { get; set; }
+    }
+}
