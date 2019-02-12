befb62c3bde6d351a6a5e9dad9b3430ed4a6c85b
mirrion <mirrion@gmail.com>
2019-02-12 16:16:23 -0200

    Colocando Permissao

--- SistemaLoja/Controllers/UsersController.cs
+++ SistemaLoja/Controllers/UsersController.cs
@@ -11,6 +11,7 @@ using System.Web.Mvc;
 
 namespace SistemaLoja.Controllers
 {
+    [Authorize(Users ="robsongasper@hotmail.com")]
     public class UsersController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
